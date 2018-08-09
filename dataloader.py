ceb574f2a56a54af68cd82ec900fd367cba464d1
Neeraj Gulia <nk.gulia@gmail.com>
2018-08-09 17:54:33 +0530

    cupy added

--- dataloader.py
+++ dataloader.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cupy as cp
 import os
 from random import shuffle
 
@@ -17,12 +18,12 @@ class Data():
 		self._load_data()
 
 	def _load_data(self):
-		datas = np.load(os.path.join(DATA_PATH, self.files[self.file_counter]))
+		datas = cp.load(os.path.join(DATA_PATH, self.files[self.file_counter]))
 		self.X = []
 		for data in datas:
 			self.X.append(data)
 		shuffle(self.X)
-		self.X = np.asarray(self.X)
+		self.X = cp.asarray(self.X)
 		self.file_counter += 1
 
 
